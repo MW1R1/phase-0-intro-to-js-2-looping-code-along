1523d7a3b8e1b0d886082fae0b186e8ccbb41db9
MW1R1 <nickmwirigi58@gmail.com>
2023-03-12 13:01:25 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function writeCards(names, text) {
+    const messages = [];
+    for (let i = 0; i < names.length; i++) {
+        const message =`Thank you, ${names[i]}, for the wonderful ${text} gift!`;
+        messages.push(message);
+    }
+
+    return messages;  
+}
+
+function countDown(){ 
+    let i = 10;  
+    while(i >= 0){
+        console.log(i--);        
+    }
+
+}
